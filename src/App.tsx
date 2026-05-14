a42149d7cce01a24e4ac1cd5dd57b5009a06adcf
rarenaam <100432085+rarenaam@users.noreply.github.com>
2026-05-14 11:52:24 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,6 @@ import Quiz from "./pages/Quiz";
 const queryClient = new QueryClient();
 
 const App = () => {
-  // De routerKey zorgt ervoor dat de router herlaadt als dat nodig is
   const [routerKey] = React.useState(() => `router-${Date.now()}`);
 
   return (
@@ -25,21 +24,19 @@ const App = () => {
       <AuthProvider>
         <TooltipProvider>
           <Toaster />
-          {/* De basename is cruciaal voor GitHub Pages om de routes te herkennen */}
           <BrowserRouter key={routerKey} basename="/physics-learning-havo-5">
             <Routes>
-              {/* Hoofdpagina */}
-              <Route path="/" element={<Index />} />
+              {/* De 'index' route wordt geladen op de basis-URL */}
+              <Route index element={<Index />} />
               
-              {/* Authenticatie Routes */}
-              <Route path="/login" element={<Login />} />
-              <Route path="/register" element={<Register />} />
-              <Route path="/forgot-password" element={<ForgotPassword />} />
-              <Route path="/reset-password" element={<ResetPassword />} />
+              {/* Relatieve paden zonder / werken beter icm basename */}
+              <Route path="login" element={<Login />} />
+              <Route path="register" element={<Register />} />
+              <Route path="forgot-password" element={<ForgotPassword />} />
+              <Route path="reset-password" element={<ResetPassword />} />
 
-              {/* Beveiligde Routes (alleen toegankelijk na inloggen) */}
               <Route 
-                path="/dashboard" 
+                path="dashboard" 
                 element={
                   <ProtectedRoute>
                     <Dashboard />
@@ -47,7 +44,7 @@ const App = () => {
                 } 
               />
               <Route 
-                path="/quiz/:topic" 
+                path="quiz/:topic" 
                 element={
                   <ProtectedRoute>
                     <Quiz />
@@ -55,7 +52,7 @@ const App = () => {
                 } 
               />
               <Route 
-                path="/quiz" 
+                path="quiz" 
                 element={
                   <ProtectedRoute>
                     <Quiz />
@@ -63,7 +60,7 @@ const App = () => {
                 } 
               />
 
-              {/* 404 Pagina voor onbekende URL's */}
+              {/* Vangt alle onbekende paden op binnen de basename */}
               <Route path="*" element={<NotFound />} />
             </Routes>
           </BrowserRouter>
